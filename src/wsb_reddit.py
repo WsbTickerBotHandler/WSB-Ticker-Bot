a029ac333d6f16e8fb637e95f4caf9ce6d0ad086
WsbTickerBotHandler <wsbtickerbothandler@gmail.com>
2020-06-01 10:00:14 -0700

    Fix bug where count() was being called instead of len()

--- src/wsb_reddit.py
+++ src/wsb_reddit.py
@@ -75,10 +75,10 @@ class WSBReddit:
         author: Redditor = item.author
         tickers: [str] = parse_tickers_from_text(body)
 
-        if tickers.count() == 0 and not item.was_comment():
+        if len(tickers) == 0 and not item.was_comment():
             logger.info(f'User {author} submitted uninterpretable message: {body}')
             notify_user_of_error(author)
-        elif tickers.count() == 0 and item.was_comment():
+        elif len(tickers) == 0 and item.was_comment():
             item.mark_read()
             return
         elif body.lower().startswith("stop"):
