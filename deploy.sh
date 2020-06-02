2f14360ff29b64d4fa53737f7be4b8e9d936c9f7
WsbTickerBotHandler <wsbtickerbothandler@gmail.com>
2020-06-01 20:07:34 -0700

    Include short git version with deployment

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,12 @@ pipenv lock -r > requirements.txt
 mkdir -p package/python
 pip install --target package/python -r requirements.txt --upgrade && \
 ARTIFACT_BUCKET=$(cat bucket-name.txt) && \
-aws cloudformation package --template-file template.yml --s3-bucket "$ARTIFACT_BUCKET" --output-template-file out.yml && \
-aws cloudformation deploy --template-file out.yml --stack-name wsb-ticker-bot --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package \
+  --template-file template.yml \
+  --s3-bucket "$ARTIFACT_BUCKET" \
+  --output-template-file out.yml && \
+aws cloudformation deploy \
+  --template-file out.yml \
+  --stack-name wsb-ticker-bot \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides Version="$(git rev-parse --short HEAD)"
