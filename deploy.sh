086a85c35f5f988a11f9e525c8f7701185aab3b9
WsbTickerBotHandler <wsbtickerbothandler@gmail.com>
2020-06-06 11:50:36 -0700

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 pipenv lock -r > requirements.txt
 mkdir -p package/python
-pip install --target package/python -r requirements.txt --upgrade && \
+pip install --target deployment/package/python -r requirements.txt --upgrade && \
 ARTIFACT_BUCKET=$(cat bucket-name.txt) && \
 aws cloudformation package \
   --template-file deployment/template.yml \
   --s3-bucket "$ARTIFACT_BUCKET" \
-  --output-template-file out.yml && \
+  --output-template-file deployment/out.yml && \
 aws cloudformation deploy \
   --template-file deployment/out.yml \
   --stack-name wsb-ticker-bot \
