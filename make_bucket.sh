858a9ddc0b682eb3006109d8b1a699d9f9220b1a
WsbTickerBotHandler <wsbtickerbothandler@gmail.com>
2020-06-10 10:40:12 -0700

    Disable bucket lifecycle policy default in case a deployment doesn't happen within 7 days

--- make_bucket.sh
+++ make_bucket.sh
@@ -3,9 +3,10 @@ BUCKET_ID=$(dd if=/dev/random bs=4 count=1 2>/dev/null | od -An -tx1 | tr -d ' \
 BUCKET_NAME=wsb-ticker-bot-lambda-artifacts-$BUCKET_ID
 echo $BUCKET_NAME > bucket-name.txt
 aws s3 mb s3://$BUCKET_NAME
-aws s3api put-public-access-block \
-    --bucket "$BUCKET_NAME" \
-    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
+# Use this to delete objects older than a set number of days
+#aws s3api put-public-access-block \
+#    --bucket "$BUCKET_NAME" \
+#    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 aws s3api put-bucket-lifecycle-configuration \
     --bucket "$BUCKET_NAME" \
     --lifecycle-configuration file://deployment/bucket-lifecycle-configuration.json
